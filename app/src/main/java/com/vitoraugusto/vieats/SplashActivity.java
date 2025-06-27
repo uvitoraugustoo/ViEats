152aeabee2e31334e3ee68dd101875f2a8f75ab9
uvitoraugustoo <vitor2008az@gmial.com>
2025-06-27 15:48:00 -0300

    Criação do projeto, da tela splash e tela de cadastro

---
+++ app/src/main/java/com/vitoraugusto/vieats/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.vitoraugusto.vieats;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView imageLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageLogo = findViewById(R.id.logo);
+        imageLogo.setAlpha(0f);
+        imageLogo.animate().alpha(1f).setDuration(500).withEndAction(() -> {
+
+            new Handler().postDelayed(() -> {
+
+                imageLogo.animate().alpha(0f).setDuration(500).withEndAction(() -> {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                });
+            }, 2000);
+        });
+    }
+}
