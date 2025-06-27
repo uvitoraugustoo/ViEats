15cee4e40e523d7df86a2e917381c3c1cb360f31
uvitoraugustoo <vitor2008az@gmial.com>
2025-06-27 16:01:39 -0300

    Criação da classe Pessoa

---
+++ app/src/main/java/com/vitoraugusto/vieats/model/Pessoa.java
@@ -0,0 +1,50 @@
+package com.vitoraugusto.vieats.model;
+
+public class Pessoa {
+    private String nome;
+    private Integer cpf;
+    private String email;
+    private Integer senha;
+
+    public Pessoa(){
+
+    }
+    public Pessoa(String nome, Integer cpf, String email, Integer senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getSenha() {
+        return senha;
+    }
+
+    public void setSenha(Integer senha) {
+        this.senha = senha;
+    }
+}
